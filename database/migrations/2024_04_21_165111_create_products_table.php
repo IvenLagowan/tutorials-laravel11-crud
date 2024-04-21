3c1da76ef8a4c213df9a16b0a24690ae5c0f6d31
chua lagowan <cuaocuq@gmail.com>
2024-04-21 10:50:47 -0700

    Web crup berhasil

---
+++ database/migrations/2024_04_21_165111_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
